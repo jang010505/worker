ecb191563b80815b5aa2c2eee3ddd4f8802915a8
징희권 <hgyellow@jinghuigwon-ui-Macmini.local>
2025-07-28 18:32:11 +0900

    Initial commit

---
+++ src/main/java/com/netflix/worker/NetflixAuthWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.netflix.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixAuthWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixAuthWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/netflix/worker/NetflixAuthWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixAuthWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
